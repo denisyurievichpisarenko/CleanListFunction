6e866d4aa036619a2d3bcb34b04a1fe83f075c24
Denis Pisarenko <42993122+denisyurievichpisarenko@users.noreply.github.com>
2023-12-11 13:15:53 +0300

    Update comment

--- cleanlist.py
+++ cleanlist.py
@@ -2,7 +2,7 @@ import re #a module for regexp
 
 def clean_list(lst): #our function
     i = 0 #the index initialized to loop through the list
-    while i < len(lst): #our loop must stop when i exceeds the length of the list
+    while i < len(lst): #our loop must stop when i exceeds the length of the list (I use while instead of for to avoid the issues with i out of range)
         if len(re.findall('[а-яА-Я]', lst[i])) > 1: #searching for cyrillic letters in the string, storing them in a list
             i += 1 #if there are more than 1 entry, we keep the string in the list, we move to the next index
         else: #if there is 1 entry or none
