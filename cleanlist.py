2a09e9d91a0939f646217d9ec6a7ea8ce50afa37
Denis Pisarenko <42993122+denisyurievichpisarenko@users.noreply.github.com>
2023-12-11 12:42:22 +0300

    Add files via upload

---
+++ cleanlist.py
@@ -0,0 +1,13 @@
+import re #a module for regexp
+
+def clean_list(lst): #our function
+    i = 0 #the index initialized to loop through the list
+    while i < len(lst): #our loop must stop when i exceeds the length of the list
+        if len(re.findall('[а-яА-Я]', lst[i])) > 1: #searching for cyrillic letters in the string, storing them in a list
+            i += 1 #if there are more than 1 entry, we keep the string in the list, we move to the next index
+        else: #if there is 1 entry or none
+            lst.pop(i) #the string is removed, i doesn't increase and we look at the new entry with this index
+    return lst #return the new list, without invalid strings
+
+#a string to test the function:
+#print(clean_list(input('Insert the strings delimited by space: ').split(' ')))
